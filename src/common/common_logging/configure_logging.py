340e2b10f623f9ed4e69d44de2eee34983ccc9b9
Damian Giebas <dgiebas@pgs-soft.com>
2021-07-05 21:10:33 +0200

    Comment out part of logging configure

--- src/common/common_logging/configure_logging.py
+++ src/common/common_logging/configure_logging.py
@@ -23,50 +23,50 @@ def configure_logging(app, name, log_level):
                 "formatter": "default",
                 "stream": "ext://sys.stdout",
             },
-            "email": {
-                "class": "logging.handlers.SMTPHandler",
-                "formatter": "default",
-                "level": log_level,
-                "mailhost": ("smtp.example.com", 587),
-                "fromaddr": "devops@example.com",
-                "toaddrs": ["receiver@example.com", "receiver2@example.com"],
-                "subject": "Error Logs",
-                "credentials": ("username", "password"),
-            },
+            # "email": {
+            #     "class": "logging.handlers.SMTPHandler",
+            #     "formatter": "default",
+            #     "level": log_level,
+            #     "mailhost": ("smtp.example.com", 587),
+            #     "fromaddr": "devops@example.com",
+            #     "toaddrs": ["receiver@example.com", "receiver2@example.com"],
+            #     "subject": "Error Logs",
+            #     "credentials": ("username", "password"),
+            # },
             # "slack": {
             #     "class": "app.HTTPSlackHandler",
             #     "formatter": "default",
             #     "level": log_level,
             # },
-            "error_file": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "formatter": "default",
-                "filename": "/var/log/gunicorn.error.log",
-                "maxBytes": 10000,
-                "backupCount": 10,
-                "delay": "True",
-            },
-            "access_file": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "formatter": "access",
-                "filename": "/var/log/gunicorn.access.log",
-                "maxBytes": 10000,
-                "backupCount": 10,
-                "delay": "True",
-            }
+            # "error_file": {
+            #     "class": "logging.handlers.RotatingFileHandler",
+            #     "formatter": "default",
+            #     "filename": "/var/log/gunicorn.error.log",
+            #     "maxBytes": 10000,
+            #     "backupCount": 10,
+            #     "delay": "True",
+            # },
+            # "access_file": {
+            #     "class": "logging.handlers.RotatingFileHandler",
+            #     "formatter": "access",
+            #     "filename": "/var/log/gunicorn.access.log",
+            #     "maxBytes": 10000,
+            #     "backupCount": 10,
+            #     "delay": "True",
+            # }
         },
-        "loggers": {
-            "gunicorn.error": {
-                "handlers": ["console"] if debug else ["console", "slack", "error_file"],
-                "level": log_level,
-                "propagate": False,
-            },
-            "gunicorn.access": {
-                "handlers": ["console"] if debug else ["console", "access_file"],
-                "level": log_level,
-                "propagate": False,
-            }
-        },
+        # "loggers": {
+        #     "gunicorn.error": {
+        #         "handlers": ["console"] if debug else ["console", "slack", "error_file"],
+        #         "level": log_level,
+        #         "propagate": False,
+        #     },
+        #     "gunicorn.access": {
+        #         "handlers": ["console"] if debug else ["console", "access_file"],
+        #         "level": log_level,
+        #         "propagate": False,
+        #     }
+        # },
         "root": {
             "level": log_level if debug else "INFO",
             "handlers": ["console"] if debug else ["console", "slack"],
