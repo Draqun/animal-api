4a1325f2c23f2a7c131d6ef85bf1338effa28263
Damian Giebas <dgiebas@pgs-soft.com>
2021-07-14 21:32:57 +0200

    Update run script

--- resources/run_gunicorn_server.sh
+++ resources/run_gunicorn_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+source /opt/upskill_backend/venv/bin/activate
+pip3 install /opt/upskill_backend/requirements.txt
 /opt/upskill_backend/venv/bin/gunicorn --pythonpath /opt/upskill_backend/src --chdir /opt/upskill_backend/src/api/webapp --log-level INFO --worker-class gevent --workers `nproc` --bind unix:upskill_backend.sock -m 007 gunicorn_entrypoint:app
